5976e78262f9b3ceeddb47b1faebfa787c6de8ad
maxarvid <max.arvid.anderson@gmail.com>
2021-07-27 17:55:17 +0200

    add fizz_buzz functionality for numbers divisible by 3

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizz_buzz' do
     it 'returns 1 if given 1' do
         expect(fizz_buzz(1)).to eq 1       
     end
+    it 'returns fizz if given 3' do
+        expect(fizz_buzz(3)).to eq 'fizz'
+    end
 end
